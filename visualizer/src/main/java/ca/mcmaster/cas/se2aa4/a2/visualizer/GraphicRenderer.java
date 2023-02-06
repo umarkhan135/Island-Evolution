c2a9aca3d42e0906b8f3692b019b37cdd4388186
Omar El Aref <o.k.elaref@gmail.com>
2023-02-06 15:53:18 -0500

    lines drawn

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.cas.se2aa4.a2.visualizer;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
@@ -9,6 +10,7 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class GraphicRenderer {
@@ -18,6 +20,22 @@ public class GraphicRenderer {
         canvas.setColor(Color.BLACK);
         Stroke stroke = new BasicStroke(0.5f);
         canvas.setStroke(stroke);
+
+        List <Vertex> points = aMesh.getVerticesList();
+        for(Segment segment : aMesh.getSegmentsList()){
+            double x1 = points.get(segment.getV1Idx()).getX();
+            double y1 = points.get(segment.getV1Idx()).getY();
+            double x2 = points.get(segment.getV2Idx()).getX();
+            double y2 = points.get(segment.getV2Idx()).getY();
+            Line2D lines = new Line2D.Double(x1,y1,x2,y2);
+            Color old = canvas.getColor();
+            //canvas.setColor(extractColor(v.getPropertiesList()));
+            canvas.draw(lines);
+
+            canvas.setColor(old);
+        }
+
+
         for (Vertex v: aMesh.getVerticesList()) {
             double centre_x = v.getX() - (THICKNESS/2.0d);
             double centre_y = v.getY() - (THICKNESS/2.0d);
@@ -26,7 +44,11 @@ public class GraphicRenderer {
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
             canvas.setColor(old);
+
+
         }
+
+
     }
 
     private Color extractColor(List<Property> properties) {
