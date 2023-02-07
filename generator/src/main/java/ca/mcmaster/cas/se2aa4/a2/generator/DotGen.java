ab60ac79ab0002b3d80df7ca942950a24ff42055
Omar El Aref <o.k.elaref@gmail.com>
2023-02-07 11:19:51 -0500

    Drawing lines

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,13 +1,14 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Random;
+import java.util.*;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+
+
 
 public class DotGen {
 
@@ -16,18 +17,46 @@ public class DotGen {
     private final int square_size = 20;
 
     public Mesh generate() {
-        Set<Vertex> vertices = new HashSet<>();
+        ArrayList<Vertex> vertices = new ArrayList<>();
+        ArrayList<Segment> line = new ArrayList<>();
         // Create all the vertices
-        for(int x = 0; x < width; x += square_size) {
-            for(int y = 0; y < height; y += square_size) {
-                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y).build());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y).build());
-                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y+square_size).build());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
+        int testing = 0;
+        int counter = 0;
+
+        for (int x = 0; x < width; x += square_size) {
+            for (int y = 0; y < height; y += square_size) {
+                vertices.add(counter, Vertex.newBuilder().setX((double) x).setY((double) y).build());
+                counter++;
+                vertices.add(counter, Vertex.newBuilder().setX((double) x + square_size).setY((double) y).build());
+                counter++;
+                vertices.add(counter, Vertex.newBuilder().setX((double) x).setY((double) y + square_size).build());
+                counter++;
+                vertices.add(counter, Vertex.newBuilder().setX((double) x + square_size).setY((double) y + square_size).build());
+                counter++;
+
+                line.add(Segment.newBuilder().setV1Idx(testing).setV2Idx(testing + 1).build());
+                line.add(Segment.newBuilder().setV1Idx(testing).setV2Idx(testing + 2).build());
+
+                if (x + square_size == width){
+                    line.add(Segment.newBuilder().setV1Idx(testing + 1).setV2Idx(testing + 3).build());
+                }
+
+                if (y + square_size == height){
+                    line.add(Segment.newBuilder().setV1Idx(testing + 2).setV2Idx(testing + 3).build());
+                }
+
+
+
+
+                testing += 4;
             }
         }
+
+
+
+
         // Distribute colors randomly. Vertices are immutable, need to enrich them
-        Set<Vertex> verticesWithColors = new HashSet<>();
+        ArrayList<Vertex> verticesWithColors = new ArrayList<>();
         Random bag = new Random();
         for(Vertex v: vertices){
             int red = bag.nextInt(255);
@@ -39,7 +68,9 @@ public class DotGen {
             verticesWithColors.add(colored);
         }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+
+
+        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(line).build();
     }
 
 }
