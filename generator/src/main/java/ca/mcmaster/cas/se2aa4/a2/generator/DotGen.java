08576be1a7266e4676726f4413694332750d4424
Omar El Aref <o.k.elaref@gmail.com>
2023-02-20 21:47:24 -0500

    almost done neighbour mesh

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -71,6 +71,87 @@ public class DotGen {
             Segment colored = Segment.newBuilder(s).addProperties(color).build();
             segmentsWithColors.add(colored);
         }
+
+        newMesh mesh = new newMesh();
+        //System.out.println(mesh.polygons);
+
+
+        List<List<Integer>> listOfNeighbors = new ArrayList<>();
+        int incrementationSize = (int) (width/square_size - 1);
+
+        List<Integer> topLeft = new ArrayList<>();
+        topLeft.add(1);
+        topLeft.add(Integer.valueOf(incrementationSize));
+        topLeft.add(Integer.valueOf(incrementationSize + 1));
+        listOfNeighbors.add(topLeft);
+
+        for (int neighborCount = 1; neighborCount < (width/square_size - 2); neighborCount++) {
+            List<Integer> newNeighborsIdx = new ArrayList<>();
+            newNeighborsIdx.add(neighborCount - 1);
+            newNeighborsIdx.add(neighborCount + 1);
+            newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize - 1));
+            newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize));
+            newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize + 1));
+            listOfNeighbors.add(newNeighborsIdx);
+        }
+
+        List<Integer> topRight = new ArrayList<>();
+        topRight.add(Integer.valueOf( (incrementationSize - 2)));
+        topRight.add(2 * Integer.valueOf( (incrementationSize - 1)));
+        topRight.add(2 * Integer.valueOf( (incrementationSize - 1)) + 1);
+        listOfNeighbors.add(topRight);
+
+        for (int neighborCount = (int) incrementationSize; neighborCount < ((incrementationSize - 1)*(incrementationSize)); neighborCount++) {
+            List<Integer> newNeighborsIdx = new ArrayList<>();
+            if(neighborCount % (width/square_size - 1) == 0){
+
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize));
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize - 1));
+                newNeighborsIdx.add(neighborCount + 1);
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize));
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize + 1));
+                listOfNeighbors.add(newNeighborsIdx);
+            }else if((neighborCount - (incrementationSize - 1)) % (incrementationSize) == 0 ){
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize));
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize + 1));
+                newNeighborsIdx.add(neighborCount - 1);
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize - 1));
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize));
+                listOfNeighbors.add(newNeighborsIdx);
+            }else{
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize + 1));
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize));
+                newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize - 1));
+                newNeighborsIdx.add(neighborCount - 1);
+                newNeighborsIdx.add(neighborCount + 1);
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize - 1));
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize));
+                newNeighborsIdx.add(neighborCount + Integer.valueOf(incrementationSize + 1));
+                listOfNeighbors.add(newNeighborsIdx);
+
+            }
+        }
+
+        List<Integer> bottomLeft = new ArrayList<>();
+        bottomLeft.add((incrementationSize * (incrementationSize - 2)));
+        bottomLeft.add(((incrementationSize) * (incrementationSize - 2)) + 1);
+        bottomLeft.add(Integer.valueOf((incrementationSize * (incrementationSize - 1)) + 1));
+        listOfNeighbors.add(bottomLeft);
+
+        for (int neighborCount = ((incrementationSize * (incrementationSize - 1)) + 1); neighborCount < ((incrementationSize * incrementationSize)-1); neighborCount++) {
+            List<Integer> newNeighborsIdx = new ArrayList<>();
+            newNeighborsIdx.add(neighborCount - 1);
+            newNeighborsIdx.add(neighborCount + 1);
+            newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize + 1));
+            newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize));
+            newNeighborsIdx.add(neighborCount - Integer.valueOf(incrementationSize - 1));
+            listOfNeighbors.add(newNeighborsIdx);
+        }
+
+
+
+        System.out.println(listOfNeighbors);
+
         return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllVertices(centroids).addAllSegments(segmentsWithColors).build();
 
     }
@@ -82,13 +163,13 @@ public class DotGen {
         String val2 = null;
         for(Property p: prop1) {
             if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
+                //System.out.println(p.getValue());
                 val1 = p.getValue();
             }
         }
         for(Property p: prop2) {
             if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
+                //System.out.println(p.getValue());
                 val2 = p.getValue();
             }
         }
@@ -108,48 +189,89 @@ public class newMesh extends DotGen{
     ArrayList<Polygon> polygons = new ArrayList<>();
         public newMesh(){
 
+            List<List<Integer>> listOfNeighbors = new ArrayList<>();
+            List<Integer> neighborsIdx = new ArrayList<>();
+            neighborsIdx.add(0);
+            neighborsIdx.add(1);
+            neighborsIdx.add(Integer.valueOf((int) (width/square_size - 1)));
+            neighborsIdx.add(Integer.valueOf((int) (width/square_size)));
+            listOfNeighbors.add(neighborsIdx);
+            neighborsIdx.clear();
+
+            for (int neighborCount = 1; neighborCount < (width/square_size - 1); neighborCount++){
+                neighborsIdx.add(neighborCount - 1);
+                neighborsIdx.add(neighborCount + 1);
+                neighborsIdx.add(neighborCount + Integer.valueOf((int) (width/square_size - 2)));
+                neighborsIdx.add(neighborCount + Integer.valueOf((int) (width/square_size - 1)));
+                neighborsIdx.add(neighborCount + Integer.valueOf((int) (width/square_size)));
+                listOfNeighbors.add(neighborsIdx);
+                neighborsIdx.clear();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            int centroidCount = 0;
             int counter = 0;
             int j = 0;
             for (int rows = 0; rows < 23; rows++) {
                 if (counter % 2 == 0) {
                     for (int i = counter * 49; i < ((counter + 1) * 49); i += 2) {
                         if (i == (46+(49*counter))){
-                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i+2).addSegmentIdxs(i+49).addSegmentIdxs(i+1).build();
+                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i+2).addSegmentIdxs(i+49).addSegmentIdxs(i+1).setCentroidIdx(centroidCount).build();
                             polygons.add(p);
+                            centroidCount++;
                         }else if(i == (48+(49*counter))){
                             continue;
                         }else {
-                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i + 3).addSegmentIdxs(i + 49).addSegmentIdxs(i + 1).build();
+                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i + 3).addSegmentIdxs(i + 49).addSegmentIdxs(i + 1).setCentroidIdx(centroidCount).build();
                             polygons.add(p);
+                            centroidCount++;
                         }
                     }
                 } else if (counter % 2 == 1) {
                     for (int i = counter * 49; i < ((counter + 1) * 49); i += 2) {
                         if (i == (46+(49*counter))){
-                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i+2).addSegmentIdxs(i+49).addSegmentIdxs(i+1).build();
+                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i+2).addSegmentIdxs(i+49).addSegmentIdxs(i+1).setCentroidIdx(centroidCount).build();
                             polygons.add(p);
+                            centroidCount++;
                         }else if(i == (48+(49*counter))){
                             continue;
                         }else{
-                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i + 3).addSegmentIdxs(i + 49).addSegmentIdxs(i + 1).build();
+                            Polygon p = Polygon.newBuilder().addSegmentIdxs(i).addSegmentIdxs(i + 3).addSegmentIdxs(i + 49).addSegmentIdxs(i + 1).setCentroidIdx(centroidCount).build();
                             polygons.add(p);
+                            centroidCount++;
                         }
                     }
                 }
                 counter++;
             }
 
-            int otherCounter = 0;
+            int otherCounter = 552;
             for (int x = 1127; x < 1173; x += 2){
-                Polygon p = Polygon.newBuilder().addSegmentIdxs(x).addSegmentIdxs(x + 3).addSegmentIdxs(x + 49 - otherCounter).addSegmentIdxs(x + 1).build();
-                otherCounter++;
+                Polygon p = Polygon.newBuilder().addSegmentIdxs(x).addSegmentIdxs(x + 3).addSegmentIdxs(x + 49 - otherCounter).addSegmentIdxs(x + 1).setCentroidIdx(otherCounter).build();
                 polygons.add(p);
+                otherCounter++;
             }
 
-            Polygon p = Polygon.newBuilder().addSegmentIdxs(1173).addSegmentIdxs(1175).addSegmentIdxs(1199).addSegmentIdxs(1174).build();
+            Polygon p = Polygon.newBuilder().addSegmentIdxs(1173).addSegmentIdxs(1175).addSegmentIdxs(1199).addSegmentIdxs(1174).setCentroidIdx(575).build();
             polygons.add(p);
 
-
+            for (Polygon poly : polygons){
+                Integer [] b = {3,4,5,6};
+                ArrayList<Integer[]> bb = new ArrayList<>();
+                bb.add(b);
+                poly.newBuilder().addAllNeighborIdxs(List.of(b)).build();
+                //System.out.println(poly.getNeighborIdxs(0));
+            }
 
         }
     }
