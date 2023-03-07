7d1d812fbf5c791c5bc1dde3c058336d1734c028
Pedro Henrique Azevedo <ped.hzazevedo@gmail.com>
2023-03-07 16:46:26 -0500

    Added island mvn directory

---
+++ island/src/main/java/ca/mcmaster/island/App.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.island;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
