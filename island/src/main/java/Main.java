cf5d05e13d0947b404af2aac0acdb47fa17a151d
Pedro Henrique Azevedo <ped.hzazevedo@gmail.com>
2023-03-08 12:02:32 -0500

    Jar creation and mesh output working

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,6 +1,17 @@
-public class Main
-{
-    public static void main( String[] args )
-    {
+import ca.mcmaster.island.Configuration.Configuration;
+import ca.mcmaster.island.islandGen;
+import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import java.io.IOException;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+
+        Configuration config = new Configuration(args);
+        Structs.Mesh aMesh = new MeshFactory().read(config.input());
+        islandGen island = new islandGen();
+        Structs.Mesh exported = island.simpleIsland(aMesh);
+        new MeshFactory().write(exported, config.output());
     }
 }
--- island/src/main/java/ca/mcmaster/island/islandGen.java
+++ island/src/main/java/ca/mcmaster/island/islandGen.java
@@ -4,7 +4,7 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 public class islandGen {
     public Structs.Mesh simpleIsland(Structs.Mesh m){
-        
+
         return m;
     }
 }
