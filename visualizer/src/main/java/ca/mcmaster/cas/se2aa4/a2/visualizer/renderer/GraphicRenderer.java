edc36160b79deb3df296393106c155a961339385
Mace-J-A <azzoparm@mcmaster.ca>
2023-03-10 11:48:19 -0500

    fo1, f02, edited ColorPropert.extract(), edited README

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/renderer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/renderer/GraphicRenderer.java
@@ -2,36 +2,47 @@ package ca.mcmaster.cas.se2aa4.a2.visualizer.renderer;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.properties.ColorProperty;
 
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Path2D;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+
+import org.apache.batik.ext.awt.image.codec.png.PNGEncodeParam.RGB;
 
 public class GraphicRenderer implements Renderer {
 
     private static final int THICKNESS = 3;
+
     public void render(Mesh aMesh, Graphics2D canvas) {
         canvas.setColor(Color.BLACK);
+        canvas.setBackground(Color.RED);
         Stroke stroke = new BasicStroke(0.2f);
         canvas.setStroke(stroke);
-        drawPolygons(aMesh,canvas);
+        drawPolygons(aMesh, canvas);
+
     }
 
     private void drawPolygons(Mesh aMesh, Graphics2D canvas) {
-        for(Structs.Polygon p: aMesh.getPolygonsList()){
+        for (Structs.Polygon p : aMesh.getPolygonsList()) {
             drawAPolygon(p, aMesh, canvas);
+
         }
     }
 
     private void drawAPolygon(Structs.Polygon p, Mesh aMesh, Graphics2D canvas) {
+        // Colour
         Hull hull = new Hull();
-        for(Integer segmentIdx: p.getSegmentIdxsList()) {
+        for (Integer segmentIdx : p.getSegmentIdxsList()) {
             hull.add(aMesh.getSegments(segmentIdx), aMesh);
         }
         Path2D path = new Path2D.Float();
@@ -45,16 +56,12 @@ public class GraphicRenderer implements Renderer {
         path.closePath();
         canvas.draw(path);
         Optional<Color> fill = new ColorProperty().extract(p.getPropertiesList());
-        if(fill.isPresent()) {
+        if (fill.isPresent()) {
             Color old = canvas.getColor();
             canvas.setColor(fill.get());
             canvas.fill(path);
             canvas.setColor(old);
         }
-    }
-
-    private void fillPolygon(Structs.Polygon p, Mesh aMesh, Graphics2D canvas){
-            
     }
 
 }
