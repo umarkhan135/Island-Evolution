ba6084005f63ac8e66556d9e452154e8943e6429
Mason-J-A <azzoparm@mcmaster.ca>
2023-03-20 11:19:29 -0400

    changed savanna color

--- island/src/main/java/ca/mcmaster/island/Tiles/LandBiomeTiles/savannaTile.java
+++ island/src/main/java/ca/mcmaster/island/Tiles/LandBiomeTiles/savannaTile.java
@@ -4,7 +4,7 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.island.Tiles.landTile;
 
 public class savannaTile extends landTile{
-    private String color_code = "17,79,14";
+    private String color_code = "110,93,59";
     public Property getColor() {
         Property p = Property.newBuilder().setKey("rgb_color").setValue(color_code).build();
         return p;
