53e046dcaef89ab4539c90b47aedde9fab7b0cdb
unknown <o.k.elaref@gmail.com>
2023-03-22 11:29:34 -0400

    temp changes

--- island/src/main/java/ca/mcmaster/island/Configuration/Configuration.java
+++ island/src/main/java/ca/mcmaster/island/Configuration/Configuration.java
@@ -7,7 +7,10 @@ public class Configuration {
     public static final String OUTPUT = "o";
     public static final String INPUT = "i";
     public static final String MODE = "m";
-
+    public static final String SHAPE = "s";
+    public static final String ALTITUDE = "altitude";
+    public static final String TEMPERATURE = "temperature";
+    public static final String PERCIPITATION = "percipitation";
     private CommandLine cli;
 
     public Configuration(String[] args) {
@@ -31,7 +34,21 @@ public class Configuration {
     }
 
     public String mode(){
-        return this.cli.getOptionValue(MODE, "lagoon");
+        return this.cli.getOptionValue(MODE, "basic");
+    }
+
+    public String shape(){
+        return this.cli.getOptionValue(SHAPE, "circle");
+    }
+
+    public String getAltitude() {
+        return this.cli.getOptionValue(ALTITUDE, "volcano");
+    }
+    public String getTemperature(){
+        return this.cli.getOptionValue(TEMPERATURE, "mild");
+    }
+    public String getPrecipitation(){
+        return this.cli.getOptionValue(PERCIPITATION, "temperate");
     }
 
     private Options options() {
@@ -39,6 +56,10 @@ public class Configuration {
         options.addOption(new Option(INPUT, true, "Input file (SVG)"));
         options.addOption(new Option(OUTPUT, true, "Output file (MESH)"));
         options.addOption(new Option(MODE, "mode", true, "Island Generation Type"));
+        options.addOption(new Option(ALTITUDE, true, "Altitude Profile"));
+        options.addOption(new Option(TEMPERATURE, true, "Whittaker Temperature Type"));
+        options.addOption(new Option(PERCIPITATION, true, "Whittaker Percipitation Type"));
+        options.addOption(new Option(SHAPE, "shape", true, "Shape of Island"));
         return options;
     }
 
